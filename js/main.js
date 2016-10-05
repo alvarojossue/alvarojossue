9eace65c977746f898bd1530cead021badcbfb21
Alvaro Castillo <alvaro.castillo23@gmail.com>
2016-10-05 00:16:59 -0400

    Change layout to mobile-first. Add different styling for header. Add animation for text-hero-area. Set the structure for a dynamic sticky navigation bar

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
 $(document).on("ready", function(){
+	
 	console.log('wired');
+	
+	$("#js-rotating").Morphext({
+    // The [in] animation type. Refer to Animate.css for a list of available animations.
+    	animation: "bounceIn",
+    // An array of phrases to rotate are created based on this separator. Change it if you wish to separate the phrases differently (e.g. So Simple | Very Doge | Much Wow | Such Cool).
+    	separator: ",",
+    // The delay between the changing of each phrase in milliseconds.
+    	speed: 2000,
+    	complete: function () {
+        // Called after the entrance animation is executed.
+    	}
+	});
+
+
+	var num = $(window).height() * 0.90 //number of pixels before modifying styles
+
+	$(window).bind('scroll', function () {
+	    if ($(window).scrollTop() > num) {
+	        $('#nav').addClass('fixed');
+	    } else {
+	        $('#nav').removeClass('fixed');
+	    }
+	});
+
 })
